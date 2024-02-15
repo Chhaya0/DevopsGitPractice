efdf85b170ef6673e189eee39db34e1f9d4214fe
Chhaya0 <68010574+Chhaya0@users.noreply.github.com>
2024-02-15 15:33:20 +0530

    Update Address.java

--- src/main/java/com/example/Address.java
+++ src/main/java/com/example/Address.java
@@ -5,18 +5,30 @@ public class Address {
     private String name;
     private String email;
     private String phone;
-	public void setName(String parameter) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void setEmail(String parameter) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void setPhone(String parameter) {
-		// TODO Auto-generated method stub
-		
-	}
 
     // constructors, getters, and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
