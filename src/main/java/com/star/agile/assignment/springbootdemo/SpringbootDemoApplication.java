fabfc9d9a818f75602ed9437d5aac0a0fb7cd604
Chhaya_P <chhayaprasad60@gmail.com>
2024-02-28 19:53:11 +0530

    edited application status

--- src/main/java/com/star/agile/assignment/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/star/agile/assignment/springbootdemo/SpringbootDemoApplication.java
@@ -2,8 +2,9 @@ package com.star.agile.assignment.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringbootDemoApplication {
 
 	public static void main(String[] args) {
